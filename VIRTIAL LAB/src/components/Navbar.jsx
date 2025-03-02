d7e95de667fbf92acbbae08b6a58beebc2297cf6
Karthikeya Raparla <kr2275@srmist.edu.in>
2025-03-02 19:34:53 +0530

    c12

--- VIRTIAL LAB/src/components/Navbar.jsx
+++ VIRTIAL LAB/src/components/Navbar.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 const logo = 'https://res.cloudinary.com/ds18h1q0k/image/upload/v1740921252/icon_o7p77j.png';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const timeoutRef = useRef(null);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleMouseEnter = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    setIsDropdownOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setIsDropdownOpen(false);
+    }, 100); // 2 seconds delay
   };
 
   return (
@@ -18,7 +33,17 @@ const Navbar = () => {
       <div className="hamburger" onClick={toggleMenu}>
       </div>
       <div className={`nav-links ${isOpen ? 'active' : ''}`}>
-        <Link to="/">About</Link>
+        <div
+          className="dropdown"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <Link to="#">About</Link>
+          <div className={`dropdown-content ${isDropdownOpen ? 'show' : ''}`}>
+            <Link to="/virtual-labs">Virtual Labs</Link>
+            <Link to="/vlead">VLEAD</Link>
+          </div>
+        </div>
         <Link to="/about">I am</Link>
         <Link to="/events">I want to</Link>
         <Link to="/schedule">Analytics</Link>
